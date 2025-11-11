a3b4110d65f7fcf41faf5e84c0fd9a6e4a1599cd
John C. G. Sturdy <jcg.sturdy@gmail.com>
2025-11-11 16:41:47 +0000

    start construcing document structure

--- litmu2latex.py
+++ litmu2latex.py
@@ -6,28 +6,65 @@ import argparse
 import os
 import re
 
-def ignore(line):
+class Visible:
+
+    def __init__(self, text):
+        self.text = text
+
+    def add(self, text):
+        self.text += text
+
+class Heading(Visible):
     pass
 
-def blank(line):
+class Rubric(Visible):
     pass
 
-def heading(line, text):
+class Verse(Visible):
+
+    def __init__(self, first, second=None, number=None):
+        self.number = number
+        self.text = first
+        self.second = second
+
+class OrderOfService:
+
+    def __init__(self):
+        self.items = []
+
+    def add(self, item):
+        self.items.append(item)
+
+    def last(self):
+        return self.items[-1]
+
+    def add_to_last(self, text):
+        self.last().add(text)
+
+def ignore(service, line):
+    pass
+
+def blank(service, line):
+    pass
+
+def heading(service, line, text):
     print("heading", text)
+    service.add(Heading(text))
 
-def rubric(line, text):
-    pass
+def rubric(service, line, text):
+    service.add(Rubric(text))
 
-def verse_initial(line, verse_number, text):
+def verse_initial(service, line, verse_number, text):
     print("verse_initial number", verse_number, "has text", text)
+    service.add(first=text, number=number)
 
-def verse_initial_divided(line, verse_number, text):
+def verse_initial_divided(service, line, verse_number, text):
     print("verse_initial number", verse_number, "has text", text, "and divider")
 
-def verse_continuation(line, text):
+def verse_continuation(service, line, text):
     print("verse_continuation text", text)
 
-def verse_continuation_divided(line, text):
+def verse_continuation_divided(service, line, text):
     print("verse_continuation text", text, "and divider")
 
 LINE_HANDLERS = [
@@ -49,13 +86,14 @@ def get_args():
     return vars(parser.parse_args())
 
 def litmu2latex_main(source, output=None, verbose=False):
+    service = OrderOfService()
     with open(source) as instream, open(output or os.path.splitext(source)[0] + ".tex", 'w') as outstream:
         for line in instream:
             line = line.rstrip()
             done = False
             for pattern, action in LINE_HANDLERS:
                 if (m := re.match(pattern, line)):
-                    action(line, *m.groups())
+                    action(service, line, *m.groups())
                     done = True
                     break
             if not done:
